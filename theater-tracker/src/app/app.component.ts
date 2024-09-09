c1c738acdbb91191c21c8c253add92703297019d
Gilbert-Three-Stars <threestarsgilbert@gmail.com>
2024-09-09 00:49:26 -0400

    integrating OpenLayers

--- theater-tracker/src/app/app.component.ts
+++ theater-tracker/src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'theater-tracker';
+  map!: Map;
+  
+  ngOnInit(): void {
+      this.map = new Map({
+        view: new View({
+          center: [0, 0],
+          zoom: 1,
+        }),
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+        ],
+        target: 'ol-map'
+      });
+  }
 }
