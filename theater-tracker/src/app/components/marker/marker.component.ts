f2b07dc1156337ffa0e655c92cbc8427e58d3ac6
Gilbert-Three-Stars <threestarsgilbert@gmail.com>
2024-09-16 20:56:53 -0400

    create marker component
    not yet functional

---
+++ theater-tracker/src/app/components/marker/marker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import MousePosition from 'ol/control/MousePosition.js';
+
+@Component({
+  selector: 'app-marker',
+  standalone: true,
+  imports: [],
+  templateUrl: './marker.component.html',
+  styleUrl: './marker.component.css'
+})
+export class MarkerComponent {
+
+}
