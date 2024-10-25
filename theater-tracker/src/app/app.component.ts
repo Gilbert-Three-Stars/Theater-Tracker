4ce08b514f8e399ef2e57f3ddf371e05a0b6decc
Gilbert-Three-Stars <threestarsgilbert@gmail.com>
2024-10-25 15:10:14 -0400

    update marker position

--- theater-tracker/src/app/app.component.ts
+++ theater-tracker/src/app/app.component.ts
@@ -12,9 +12,10 @@ import Feature from 'ol/Feature.js';
 import Style from 'ol/style/Style.js';
 import Icon from 'ol/style/Icon.js'
 import Point from 'ol/geom/Point.js';
-import { LocationService } from './services/location.service';
-import Collection from 'ol/Collection';
+import { GeolocationService } from '@ng-web-apis/geolocation';
+import { take } from 'rxjs/operators';
 import { fromLonLat } from 'ol/proj';
+import View from 'ol/View';
 
 
 
@@ -29,15 +30,20 @@ import { fromLonLat } from 'ol/proj';
 export class AppComponent implements OnInit {
   title = 'theater-tracker';
   map!: Map;
+  constructor(private readonly geolocation$: GeolocationService) {}
   
   ngOnInit(): void {
-    let locService = new LocationService;
-    let curView = locService.getView();
-    console.log("cur view center: " + curView.getCenter())
-    let curCoords = curView.getCenter();
+    let curCoords = fromLonLat([-71.06, 42.36]);
+    let curView = new View();
+    curView.setZoom(5);
+    this.geolocation$.pipe(take(1)).subscribe((position): void => {
+      curCoords = fromLonLat([position.coords.longitude, position.coords.latitude])
+      curView.setZoom(12);
+    })
+    curView.setCenter(curCoords)
     console.log(curCoords)
     let curLocationFeature = new Feature({
-      geometry: new Point(locService.getCoords('EPSG:3857')),
+      geometry: new Point(curCoords),
     })
     let markerLayer = new VectorLayer({
       source: new VectorSource({
@@ -45,7 +51,7 @@ export class AppComponent implements OnInit {
       }),
       style: new Style({
         image: new Icon({
-          anchor: [0.5, 1],
+          anchor: [0.5, 0.8],
           anchorXUnits: 'fraction',
           anchorYUnits: 'fraction',
           crossOrigin: 'anonymous',
--- theater-tracker/src/app/services/location.service.ts
+++ theater-tracker/src/app/services/location.service.ts
@@ -3,29 +3,25 @@ import View from 'ol/View';
 import {transform} from 'ol/proj';
 import { fromLonLat } from 'ol/proj';
 import { ProjectionLike } from 'ol/proj';
+import { GeolocationService } from '@ng-web-apis/geolocation';
+import { take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
   protected view = new View();
-  protected _viewCenter = [-7912769.528381, 5215479.987170]
-  constructor() { 
-    this.view.setZoom(12);
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position : GeolocationPosition) => {
-        this._viewCenter = fromLonLat([position.coords.longitude, position.coords.latitude], 'EPSG:3857')
-        this.view.setCenter(this._viewCenter)
-      }, 
-      (err: GeolocationPositionError) => {
-        console.warn(err.message)
-        this.view.setCenter(this._viewCenter)
-      })
-    }
-    else {
-      console.warn("Couldn't get navigator.geolocation")
-      this.view.setCenter(this._viewCenter);
+  protected _viewCenter = fromLonLat([-71.06, 42.36]);
+  private locAccuracy = 0; 
+  constructor(private readonly geolocation$: GeolocationService) { 
+    
+    this.geolocation$.pipe(take(1)).subscribe((position) => {
+      this._viewCenter = fromLonLat([position.coords.longitude, position.coords.latitude]);
+      this.locAccuracy = 2;
+    })
+    if(this.locAccuracy === 0) {
+      const ipURL = new URL('http://ip-api.com/json/');
+      
     }
   }
   getView() : View {
