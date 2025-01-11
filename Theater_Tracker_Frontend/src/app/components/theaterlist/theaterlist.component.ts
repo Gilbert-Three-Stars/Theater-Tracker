3a9f7c8e7855d6d18bdbecfc726e6b7d1a2de8d6
Gilbert Three Stars <threestarsgilbert@gmail.com>
2025-01-11 17:15:18 -0500

    pagination working properly

--- Theater_Tracker_Frontend/src/app/components/theaterlist/theaterlist.component.ts
+++ Theater_Tracker_Frontend/src/app/components/theaterlist/theaterlist.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, ViewChild } from '@angular/core';
 import { Theater } from '../../models/theater.model';
 import { CommonModule,  } from '@angular/common';
 import { MatTable, MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-theaterlist',
@@ -13,12 +13,32 @@ import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 })
 export class TheaterlistComponent {
   @Input() nearbyTheaters: [Theater, number][] = [];
-  theaterDataSource = new MatTableDataSource(this.nearbyTheaters);
+  theaterDataSource = new MatTableDataSource<[Theater, number]>([]);
+  pageSize = 5;
+  curPageIndex = 0;
   @ViewChild(MatTable) theaterTable!: MatTable<any>;
 
   displayedColumns = ['name', 'address'];
-  ngOnInit(): void {
+  // since our only input is nearbyTheaters, and nearbyTheaters changes as a result of the button being
+  // pressed, this happens when the button gets pressed. We want to reset the curPageIndex.
+  ngOnChanges(): void {
+    console.log('should only occur on button press');
+    let curTheaterArr = []
+    this.curPageIndex = 0;
+    for(let i = 0; i < Math.min(this.pageSize, this.nearbyTheaters.length); i++) {
+      curTheaterArr.push(this.nearbyTheaters[i]);
+    }
+    this.theaterDataSource.data = curTheaterArr;
     this.theaterTable.renderRows();
   }
-  //TODO: Get pagination working properly
+
+  pageChangeEvent(event: PageEvent) {
+    this.curPageIndex = event.pageIndex;
+    let curTheaterArr = [];
+    for(let i = this.curPageIndex * 5; i < Math.min(this.pageSize * (this.curPageIndex + 1), this.nearbyTheaters.length); i++) {
+      curTheaterArr.push(this.nearbyTheaters[i]);
+    }
+    this.theaterDataSource.data = curTheaterArr;
+    this.theaterTable.renderRows();
+  }
 }
